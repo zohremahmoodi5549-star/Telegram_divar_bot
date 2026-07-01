3d6a1e7ae63d3bc4027060eccb532d74bfb84b2b
zohremahmoodi5549-star <zohremahmoodi.5549@gmail.com>
2026-07-01 18:27:21 +0330

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,18 @@
-print("Bot Started")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = "8862543927:AAFab0buerlXtECOiqOgl-BUA5NS6uMlhg8"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! ربات با موفقیت فعال شد. 👋")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+print("Bot Started...")
+app.run_polling()
